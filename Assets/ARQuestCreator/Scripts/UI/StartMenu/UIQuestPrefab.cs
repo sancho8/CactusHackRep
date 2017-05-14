2b479becc9bbe01d7e1f1c608027abe092cbb151
STARasGAMES <doterpoter@gmail.com>
2017-05-14 03:52:44 +0300

    Add quest item UI

---
+++ Assets/ARQuestCreator/Scripts/UI/StartMenu/UIQuestPrefab.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace ARQuestCreator.UI
+{
+    public class UIQuestPrefab : MonoBehaviour, IUIController
+    {
+
+        [SerializeField] Text _questNameText;
+        private string _name;
+
+
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(OnClick);
+        }
+
+
+        public void SetQuest(string name)
+        {
+            _questNameText.text = name;
+            _name = name;
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        void OnClick()
+        {
+            Debug.Log("AAAAAAAAAAAAAA");
+        }
+
+
+    }
+}
