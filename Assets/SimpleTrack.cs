52e371d21d8c905485a50e8d18de0e68e9e539cb
STARasGAMES <doterpoter@gmail.com>
2017-05-14 13:00:06 +0300

    Simple track

---
+++ Assets/SimpleTrack.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class SimpleTrack : MonoBehaviour, ITrackableEventHandler {
+
+    #region PRIVATE_MEMBER_VARIABLES
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    #endregion // PRIVATE_MEMBER_VARIABLES
+
+    [SerializeField] GameObject scene;
+
+    #region UNTIY_MONOBEHAVIOUR_METHODS
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    /// Implementation of the ITrackableEventHandler function called when the
+    /// tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+
+
+    #region PRIVATE_METHODS
+
+
+    private void OnTrackingFound()
+    {
+        scene.SetActive(true);
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+    }
+
+
+    private void OnTrackingLost()
+    {
+        scene.SetActive(false);
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+    }
+
+    #endregion // PRIVATE_METHODS
+}
